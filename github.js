a408a937a31883bef16d8d5d0910f2c4d315acfb
Ethan Armbrust <ethanarm@buffalo.edu>
2019-04-25 15:27:22 -0400

    Working on favorite languages

--- github.js
+++ github.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
     $('#username').val("");
     $.ajax({
       url: `https://api.github.com/users/${username}/repos`,
-      //url: `http://api.openweathermap.org/data/2.5/weather?q=${city}`,
       type: 'GET', // GET because we're passing parameters in the URL
       data: {
         format: 'json'
@@ -16,14 +15,24 @@ $(document).ready(function() {
       success: function(response) {
         let repo_count = response.length;
         let language_list = "";
+        let language_arr = [];
         for(var i = 0; i < repo_count; i++){
           language_list += response[i].name;
           language_list += '\n';
           language_list += '   ' + response[i].language;
+          language_arr.push(response[i].language);
           language_list += '\n';
         }
+        var unique_lan = [];
+        for(var i = 0; i < repo_count; i++){
+          if(!unique_lan.includes(language_arr[i])){
+            unique_lan.push(language_arr[i]);
+          }
+        }
+        console.log(unique_lan);
         $('.repoCount').text(`${username} has ${repo_count} public repositories.`)
         $('.showHumidity').text(`The first listed repo of ${username} is ${response[0].name}`);
+        $('.favorite').text('Users favorite language: ')
         $('.showTemp').html(language_list).wrap('<pre />');
       },
       error: function(response) {
